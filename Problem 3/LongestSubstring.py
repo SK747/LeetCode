54ed859f59ee61d93ca44f10b9bfc2d655f6263f
Shaheer <42694231+SK747@users.noreply.github.com>
2021-12-12 02:06:39 +0500

    Update LongestSubstring.py

--- Problem 3/LongestSubstring.py
+++ Problem 3/LongestSubstring.py
@@ -1,1 +1,23 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        templist = ''
+        longest = 0
+        for j in range(len(s)):
+            for i in range(j,len(s)):
+                if s[i] in templist:
+                    if len(templist) >= longest:
+                        longest = len(templist)
+                    templist = s[i]
+                else:
+                    templist=templist + s[i]
+                print(templist)
+            print('first loop done')
+            if len(templist) >= longest:
+                longest = len(templist)
+            templist = ''
+        return longest
+        
 
+s = Solution()
+input = "qrsvbspk"
+print(s.lengthOfLongestSubstring(input))
