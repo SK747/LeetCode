16815becbc4727f58cca7f6304d59a13530f3510
Shaheer <42694231+SK747@users.noreply.github.com>
2021-12-14 03:51:27 +0500

    new

--- Problem 1/TwoSum.py
+++ Problem 1/TwoSum.py
@@ -5,8 +5,6 @@ class Solution:
                 if nums[i] + nums[j] == target and i != j:
                     return [i,j]
 
-
-
 s = Solution()
 input = [1,2,3,4,5,6,7]
 print(s.twoSum(input, 9))
---
+++ Problem 771/JewelsandStones.py
@@ -0,0 +1,8 @@
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        count = 0
+        for c in range(len(stones)):
+            for d in range(len(jewels)):
+                if jewels[d] == stones[c]:
+                    count += 1
+        return count
